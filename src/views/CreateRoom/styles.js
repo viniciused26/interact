d2c61f72729ce5d55a70a6bd313103e2d003b16f
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-10-16 16:58:23 -0300

    Creating page CreateRoom

---
+++ src/views/CreateRoom/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+width: 100%;
+height: 100vh;
+background: #1A1A2E;
+
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+
+input {
+    margin-bottom: 25px;
+    color: #1A1A2E;
+    font-size: 16px;
+    width: 260px;
+    height: 50px;
+    border-radius: 8px;
+    padding: 0 16px;
+    background: #fff;
+    border: 2px solid #1A1A2E;
+}
+
+input::placeholder {
+    color: #16213E;
+}
+
+span {
+    margin-bottom: 25px;
+    text-align: center;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+
+}
+`
--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,37 +1,19 @@
 import React from 'react'
 import * as S from './styles'
 import { useHistory } from 'react-router-dom'
-
 import Button from '../../components/Button'
 import api from '../../services/Api';
 
 function Homepage() {
   const history = useHistory()
-  const [sala, setSala] = React.useState();
-  const firstUpdate = React.useRef(true);
 
-
-  React.useLayoutEffect(() => {
-    if (firstUpdate.current) firstUpdate.current = false;
-    else history.push(`/rooms/adm/${sala}`)
-    
-  }, [sala])
+  function navigateToCreateRoom() {
+    history.push('/rooms/Create')
+  }
 
   function navigateToJoinRoom() {
     history.push('/rooms/enter')
   }
-  
-
-  const navigateToCreateRoom = React.useCallback(() => {
-    if(localStorage.getItem('id_usuario'))
-      api.post('/salas', {id_moderador: localStorage.getItem('id_usuario')}).then(response => setSala(response.data.id_sala))
-    else
-      api.post('/usuarios', {}).then(response => {
-        localStorage.setItem('id_usuario', response.data.id_usuario)
-        api.post('/salas', {id_moderador: response.data.id_usuario}).then(response => setSala(response.data.id_sala))
-      })
-    
-  })
 
   return (
     <S.Container>
