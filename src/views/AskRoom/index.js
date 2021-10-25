296e100c05746803cb28eb95eeeb699ceebfe6ea
Peh099 <70337717+Peh099@users.noreply.github.com>
2021-10-25 16:26:56 -0300

    Atualizacao nome do host

--- src/views/AskRoom/index.js
+++ src/views/AskRoom/index.js
@@ -13,21 +13,22 @@ function AskRoom(props) {
   const [texto, setTexto] = React.useState()
   const history = useHistory()
 
-
   const navigateToHomepage = React.useCallback(() => {
-    api.put(`/usuarios/${localStorage.getItem('id_usuario')}`, { id_sala: null })
+    api.put(`/usuarios/${localStorage.getItem('id_usuario')}`, {
+      id_sala: null
+    })
     history.push('/')
   })
 
-  const MINUTE_MS = 1000;
+  const MINUTE_MS = 1000
 
   React.useEffect(() => {
     const interval = setInterval(() => {
       const id_sala = props.match.params.code
       api.get(`/salas/${id_sala}`, {}).then(response => setSala(response.data))
-    }, MINUTE_MS);
+    }, MINUTE_MS)
 
-    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    return () => clearInterval(interval) // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
   }, [])
 
   function handleChange(event) {
@@ -35,7 +36,11 @@ function AskRoom(props) {
   }
 
   const handleSubmit = React.useCallback(() => {
-    api.post(`/perguntas`, { id_usuario: localStorage.getItem('id_usuario'), id_sala: props.match.params.code, conteudo: texto })
+    api.post(`/perguntas`, {
+      id_usuario: localStorage.getItem('id_usuario'),
+      id_sala: props.match.params.code,
+      conteudo: texto
+    })
   })
 
   function sortQuestions(questions) {
@@ -55,39 +60,91 @@ function AskRoom(props) {
   }
 
   function clearField() {
-    document.getElementById('messageBar').value = "";
+    document.getElementById('messageBar').value = ''
   }
 
   return (
     <S.Container>
-      <Header height='400px' text={sala ? sala.perguntas.map(pergunta => {
-        if (pergunta.is_respondida)
-          return <QuestionCard name={"Nome do usuário Aqui"} id={pergunta.id_pergunta} isVoted={isVoted(pergunta)} upvotes={pergunta.concordaram.length} isModerator={false} text={pergunta.conteudo} isSmall={true} />
-      }) : null} isModerator={false} navigateToHomepage={navigateToHomepage} roomName="Nome da Sala Aqui" />
+      <Header
+        height="400px"
+        text={
+          sala
+            ? sala.perguntas.map(pergunta => {
+                if (pergunta.is_respondida)
+                  return (
+                    <QuestionCard
+                      name={'Nome do usuário Aqui'}
+                      id={pergunta.id_pergunta}
+                      isVoted={isVoted(pergunta)}
+                      upvotes={pergunta.concordaram.length}
+                      isModerator={false}
+                      text={pergunta.conteudo}
+                      isSmall={true}
+                    />
+                  )
+              })
+            : null
+        }
+        isModerator={false}
+        navigateToHomepage={navigateToHomepage}
+        roomName="Nome da Sala Aqui"
+      />
 
       <S.LeftSide>
-        <h1>Perguntas Mais Votadas</h1>
-        {sala ? sortQuestions(sala.perguntas).map(pergunta => {
-          if (!pergunta.is_respondida)
-            return <QuestionCard name={"Nome do usuário Aqui"} id={pergunta.id_pergunta} isVoted={isVoted(pergunta)} upvotes={pergunta.concordaram.length} isModerator={false} text={pergunta.conteudo} isSmall={true} />
-        }) : null}
+        <div id="rectangle">Nome do host</div>
+        {sala
+          ? sortQuestions(sala.perguntas).map(pergunta => {
+              if (!pergunta.is_respondida)
+                return (
+                  <QuestionCard
+                    name={'Nome do usuário Aqui'}
+                    id={pergunta.id_pergunta}
+                    isVoted={isVoted(pergunta)}
+                    upvotes={pergunta.concordaram.length}
+                    isModerator={false}
+                    text={pergunta.conteudo}
+                    isSmall={true}
+                  />
+                )
+            })
+          : null}
       </S.LeftSide>
 
       <S.RightSide>
-
-        {sala ? sala.perguntas.map(pergunta => {
-          if (!pergunta.is_respondida)
-            return <QuestionCard name={"Nome do usuário Aqui"} id={pergunta.id_pergunta} isVoted={isVoted(pergunta)} upvotes={pergunta.concordaram.length} isModerator={false} text={pergunta.conteudo} isSmall={true} />
-        }) : null}
+        {sala
+          ? sala.perguntas.map(pergunta => {
+              if (!pergunta.is_respondida)
+                return (
+                  <QuestionCard
+                    name={'Nome do usuário Aqui'}
+                    id={pergunta.id_pergunta}
+                    isVoted={isVoted(pergunta)}
+                    upvotes={pergunta.concordaram.length}
+                    isModerator={false}
+                    text={pergunta.conteudo}
+                    isSmall={true}
+                  />
+                )
+            })
+          : null}
       </S.RightSide>
 
       <S.Bottom>
-        <input id="messageBar" type="text" placeholder="Digite aqui sua pergunta" onChange={handleChange} />
-        <SmallButton onClick={() => { handleSubmit(); clearField(); }} color={'#E94560'} title={'Enviar Pergunta'} />
+        <input
+          id="messageBar"
+          type="text"
+          placeholder="Digite aqui sua pergunta"
+          onChange={handleChange}
+        />
+        <SmallButton
+          onClick={() => {
+            handleSubmit()
+            clearField()
+          }}
+          color={'#E94560'}
+          title={'Enviar Pergunta'}
+        />
       </S.Bottom>
-
-
-
     </S.Container>
   )
 }
