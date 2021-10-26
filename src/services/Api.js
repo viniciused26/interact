3b216aa2e4ce1ae2638660ffc82485c408606a15
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-25 23:16:59 -0300

    Mudando api para producao

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const PROD = 'https://interact-api.herokuapp.com/'
 const DEV = 'http://192.168.1.3:5000/'
 const api = axios.create({
-  baseURL: DEV,
+  baseURL: PROD,
 });
 
 
