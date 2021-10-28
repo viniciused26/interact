780a2b26d07966286b3069646858ff0aa593bd86
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-10-28 12:34:54 -0300

    Mudando a constDEv para localhost 5000

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const PROD = 'https://interact-api.herokuapp.com/'
-const DEV = 'http://192.168.1.3:5000/'
+const DEV = 'localhost:5000'
 const api = axios.create({
   baseURL: 'https://interact-api.herokuapp.com/',
 });
